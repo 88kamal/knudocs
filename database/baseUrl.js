e6f9cd9256bea5b7ea68be1d04a70febe94501c2
KAMAL NAYAN UPADHYAY <knupadhyay784@gmail.com>
2023-02-04 01:31:49 +0530

    okk

--- database/baseUrl.js
+++ database/baseUrl.js
@@ -1,3 +1,3 @@
-const baseUrl = process.env.NODE_ENV === 'production' ? "https://knudocs.vercel.app" : "http://localhost:3000"
+const baseUrl = process.env.NODE_ENV === 'production' ? "https://knudocs-88kamal.vercel.app" : "http://localhost:3000"
 
 export default baseUrl
